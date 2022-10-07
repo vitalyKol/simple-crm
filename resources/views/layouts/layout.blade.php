a86cbdf9d2080f3c3d91bef86e99bde31f8e0e72
vitalyKol <vental-kvo@yandex.ru>
2022-10-07 09:03:51 +0300

    add view of top line

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -24,7 +24,27 @@
                 <li><a href="#"><i class="fa fa-tasks" aria-hidden="true"></i> Tasks</a></li>
             </ul>
         </div>
-        <div class="col-sm-9 col-lg-10"><i class="fa fa-bars" aria-hidden="true"></i>Right panel</div>
+        <div class="col-sm-9 col-lg-10 right_block">
+            <div class="top_line">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+                <div class="top_line_login">
+                    @if (Route::has('login'))
+                        @auth
+                            <a href="{{ url('/dashboard') }}">Dashboard</a>
+                        @else
+                            <a href="{{ route('login') }}">Log in</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">| Register</a>
+                            @endif
+                        @endauth
+                    @endif
+                </div>
+            </div>
+            <div class="content">
+                Right panel
+            </div>
+        </div>
     </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
