1fae8ec379f2cb3f2845b62c4f38ed6998097da0
vitalyKol <vental-kvo@yandex.ru>
2022-10-07 08:44:40 +0300

    add view of left sidebar

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,32 @@
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
+          integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3 col-lg-2 left_menu">
+            <h5 class="text-center left_menu_title">CRM</h5>
+            <ul class="left_menu_list">
+                <li><a href="#"><i class="fa fa-list-alt" aria-hidden="true"></i> Dashboard</a></li>
+                <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i> Users</a></li>
+                <li><a href="#"><i class="fa fa-address-card" aria-hidden="true"></i> Clients</a></li>
+                <li><a href="#"><i class="fa fa-folder-open" aria-hidden="true"></i> Projects</a></li>
+                <li><a href="#"><i class="fa fa-tasks" aria-hidden="true"></i> Tasks</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-9 col-lg-10"><i class="fa fa-bars" aria-hidden="true"></i>Right panel</div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/main', function(){
+    return view('main');
+});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
