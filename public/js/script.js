a2500b75a0f0c3d737f91ea694d6349138707a97
vitalyKol <vental-kvo@yandex.ru>
2022-12-05 10:16:37 +0300

    fix hide/open effect for side menu

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,3 @@
-let sidebar = document.getElementById('switch_sidebar');
-sidebar.addEventListener('click', getLifeSidebar);
-
-function getLifeSidebar(){
-    let menu = document.getElementById('side_menu');
-    menu.classList.toggle('switch_menu');
-    let spans = document.querySelectorAll('#side_menu span');
-    spans.forEach(function(item){
-        item.classList.toggle('d-none');
-    });
-}
-
+$('#switch_sidebar').click(function (){
+    $('#side_menu').toggle(500);
+})
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -43,7 +43,7 @@
                     @endif
                 </div>
             </div>
-            <div class="container content">
+            <div class="container content" id="content">
                 <div class="row">
                     <div class="col">
                         @yield('content')
@@ -55,6 +55,7 @@
     </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 <script src="{{asset('/js/script.js')}}"></script>
 </body>
 </html>
